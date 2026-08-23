f7ec72e118730cb90ab44321e88a78d5366ef83c
Gutha Sushma Sree <guthasushmasree318@gmail.com>
2026-08-23 16:52:50 +0530

    practical 1 code

--- 2520030098_Practical/Practical-01/practical1.c
+++ 2520030098_Practical/Practical-01/practical1.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main(){
+int pid;
+char buff[50];
+printf("Enter Linux Command:");
+scanf("%s",buff);
+pid=fork();
+if(pid==0){
+printf("Child Process\n");
+printf("Child PID: %d\n",getpid());
+printf("Parent PID: %d\n",getppid());
+execlp(buff,buff,NULL);
+}
+else if(pid>0){
+printf("Parent Process\n");
+printf("Parent PID: %d\n",getpid());
+wait(NULL);
+printf("Child Process Completed\n");
+}
+else{
+printf("Fork failed\n");
+}
+return 0;
+}
